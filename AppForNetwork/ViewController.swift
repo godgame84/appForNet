fbbaecf89008d8482fe1518a873577a633ebff47
Ivan <godgame84@icloud.com>
2020-06-24 11:15:19 +0300

    Initial Commit

---
+++ AppForNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppForNetwork
+//
+//  Created by Иван Лебедев on 24.06.2020.
+//  Copyright © 2020 Иван Лебедев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
